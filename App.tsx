28a8204b754bd08dfbe4ea5685e6a2d28f527b43
Lucca Rodrigues <contato.luccarodrigues@gmail.com>
2021-04-20 14:26:20 -0300

    add confirmation page

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Welcome } from './src/pages/welcome';
 import { UserIdentification } from './src/pages/userIdentification';
+import { Confirmation } from './src/pages/confirmation';
 import AppLoading from 'expo-app-loading';
 
 import {
@@ -18,6 +19,6 @@ export default function App() {
   if(!fontsLoaded) return <AppLoading />
 
   return (
-    <UserIdentification/>
+    <Confirmation/>
   );
 }
---
+++ src/pages/confirmation.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Text, View, SafeAreaView, StyleSheet, Dimensions } from 'react-native';
+
+import {Button} from '../components/button';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+export function Confirmation() {
+  return(
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.emoji}>
+          😄
+        </Text>
+        <Text style={styles.title}>
+          Prontinho!
+        </Text>
+        <Text style={styles.subtitle}>
+          Agora vamos começar a cuidar das suas plantinhas com muito cuidado.
+        </Text>
+        <View style={styles.footer}>
+          <Button title="Confirmar"/>
+        </View>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    padding: 30
+  },
+  emoji: {
+    fontSize: 78
+  },
+  title: {
+    fontSize: 22,
+    fontFamily: fonts.heading,
+    textAlign: 'center',
+    color: colors.heading,
+    lineHeight: 38,
+    marginTop: 15
+  },
+  subtitle: {
+    fontFamily: fonts.text,
+    textAlign: 'center',
+    fontSize: 17,
+    paddingVertical: 15,
+    color: colors.heading
+  },
+  footer: {
+    width: '100%',
+    paddingHorizontal: Dimensions.get('window').width * 0.1,
+    marginTop: 20
+  }
+})
